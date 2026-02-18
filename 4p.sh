fd57ca22b22d00c2ca241cd40350c888ba67091c
vasyakin <54988266+vasyakin@users.noreply.github.com>
2026-02-18 17:53:11 +1000

    Update 4p.sh
    Заменил ссылку на обновление скрипта, чтобы не возвращались иероглифы

--- 4p.sh
+++ 4p.sh
@@ -4625,7 +4625,7 @@ rm /tmp/crontab.tmp
 
 lnsb(){
 rm -rf /usr/bin/sb
-curl -L -o /usr/bin/sb -# --retry 2 --insecure https://raw.githubusercontent.com/yonggekkk/sing-box-yg/main/sb.sh
+curl -L -o /usr/bin/sb -# --retry 2 --insecure https://raw.githubusercontent.com/vasyakin/4protocols/refs/heads/main/4p.sh
 chmod +x /usr/bin/sb
 }
 
@@ -4713,7 +4713,7 @@ iptables -t nat -F PREROUTING >/dev/null 2>&1
 netfilter-persistent save >/dev/null 2>&1
 service iptables save >/dev/null 2>&1
 green "Удаление Sing-box завершено!"
-blue "Добро пожаловать в использование скрипта Sing-box-yg: bash <(curl -Ls https://raw.githubusercontent.com/yonggekkk/sing-box-yg/main/sb.sh)"
+blue "Добро пожаловать в использование скрипта Sing-box-yg: bash <(curl -Ls https://raw.githubusercontent.com/vasyakin/4protocols/refs/heads/main/4p.sh)"
 echo
 }
 
